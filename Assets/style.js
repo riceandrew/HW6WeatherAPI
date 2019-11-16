235c417b3dfba523f2fc182e82697bfd18866b9b
Andrew Rice <riceandrew8@gmail.com>
2019-11-16 11:09:06 -0600

    Updated weather response

---
+++ Assets/style.js
@@ -0,0 +1,40 @@
+
+
+function getWeather() {
+    $('.weatherResponse').html('');
+    var cityName = $('#cityName').val();
+    localStorage.setItem("cityInfo", cityName);
+    var apiCall = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=b6fa23b52bf3cf09a13463a9202b84e8';
+
+    // $.getJSON(apiCall, weatherCallback);
+
+    $.ajax({
+        type: "GET",
+        url: apiCall,
+        dataType: "json",
+    }).then(function(weatherData){
+        console.log(weatherData)
+        var cityName = weatherData.name;
+        var description = weatherData.weather[0].description;
+        var temp = weatherData.main.temp;
+        var humid = weatherData.main.humidity;
+        var wind = weatherData.wind.speed;
+        var cityInfo = cityName + description + temp + humid + wind;
+        $('.weatherResponse').append("<H2>" + cityName + "<H2>");
+        $('.weatherResponse').append("<h3>" + "Current Weather: " + description + "<h3>");
+        $('.weatherResponse').append("<h3>" + "Current Temperature: " + temp + "<h3>");
+        $('.weatherResponse').append("<h3>" + "Humidity: " + humid + "<h3>");
+        $('.weatherResponse').append("<h3>" + "Wind Speed : " + wind + "<h3>");
+    })
+
+
+    // function weatherCallback(weatherData) {
+
+
+       
+    // }
+
+}
+
+
+
