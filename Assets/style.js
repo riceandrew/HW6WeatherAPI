29a55243bb9abded8d85249d29050ef29db17923
Andrew Rice <riceandrew8@gmail.com>
2019-11-16 12:30:06 -0600

    working local storage

--- Assets/style.js
+++ Assets/style.js
@@ -1,13 +1,12 @@
 
-
+let history = []
 function getWeather() {
     $('.weatherResponse').html('');
     var cityName = $('#cityName').val();
-    localStorage.setItem("cityInfo", cityName);
+    history.push(cityName);
+    localStorage.setItem("History", JSON.stringify(history));
     var apiCall = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=b6fa23b52bf3cf09a13463a9202b84e8';
-
     // $.getJSON(apiCall, weatherCallback);
-
     $.ajax({
         type: "GET",
         url: apiCall,
@@ -25,16 +24,19 @@ function getWeather() {
         $('.weatherResponse').append("<h3>" + "Current Temperature: " + temp + "<h3>");
         $('.weatherResponse').append("<h3>" + "Humidity: " + humid + "<h3>");
         $('.weatherResponse').append("<h3>" + "Wind Speed : " + wind + "<h3>");
+        loopLocalStorage()
     })
-
-
-    // function weatherCallback(weatherData) {
-
-
-       
+    
+    // function weatherCallback(weatherData) {   
     // }
-
 }
-
-
-
+function loopLocalStorage (){
+    console.log("HIST", window.localStorage.getItem("History"))
+    let hist = JSON.parse(window.localStorage.getItem("History")) || []; // "[Austin, Houston]" ----> [Austin, Houston]
+    
+    for (var i =0; i < hist.length; i++){
+       var apiSearch=  'http://api.openweathermap.org/data/2.5/weather?q=' + hist[i] + '&appid=b6fa23b52bf3cf09a13463a9202b84e8';
+       console.log("API", apiSearch)
+    }
+}
+loopLocalStorage()
